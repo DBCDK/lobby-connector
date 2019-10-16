ecfd7bd84cee576e5a8c535ff2d33af92d447949
Anders Thrane Michelsen <atm@dbc.dk>
2019-10-16 08:06:09 +0200

    Add basic files
    Auditors: jbn, jbr

---
+++ src/main/java/dk/dbc/lobby/LobbyConnector.java
@@ -0,0 +1,4 @@
+package dk.dbc.lobby;
+
+public class LobbyConnector {
+}
---
+++ src/test/java/dk/dbc/lobby/LobbyConnectorTest.java
@@ -0,0 +1,11 @@
+package dk.dbc.lobby;
+
+import org.junit.jupiter.api.Test;
+
+public class LobbyConnectorTest {
+
+    @Test
+    public void testDummy() {
+
+    }
+}
